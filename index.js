9a372712fbb79bb93612c406a2ba470174a20a25
Abhishek Shrivastava <abhishekshrivastava78@gmail.com>
2020-07-13 09:54:16 +0000

    finished

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
+// We define an hourglass in  to be a subset of values with indices falling in this pattern in 's graphical representation:
+
+// a b c
+//   d
+// e f g
+// There are  hourglasses in , and an hourglass sum is the sum of an hourglass' values.
+
+// Task
+// Calculate the hourglass sum for every hourglass in , then print the maximum hourglass sum.
 function maxHourGlassSum(arr){
 let col=arr[0].length;
 let row=arr.length;
-console.log("row="+row);
-console.log("col="+col);
 let harr=[];
 for(let i=0;i<row-3;i++){
   for(let j=0;j<col-3;j++){
     harr.push(getHourGlassCount(i,j,arr));
 }
 }
-console.log("harr="+harr);
+
 return Math.max(...harr);
 
 }
